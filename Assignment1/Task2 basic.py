17b3e578f046836eb31b10bc7bd7d1ba53fe7955
Rishitha-1320 <srishitha332@gmail.com>
2025-06-05 12:36:39 +0530

    Add files via upload

---
+++ Assignment1/Task2 basic.py
@@ -0,0 +1,4 @@
+first_name = input("Enter your first name: ")
+last_name = input("Enter your last name: ")
+full_name = f"{first_name} {last_name}"
+print(f"Hello, {full_name}! Welcome to Python program.")
---
+++ Assignment1/task1basic.py
@@ -0,0 +1,10 @@
+a=int(input("Enter the first number="))
+b=int(input("Enter the second number="))
+ad=a+b
+su=a-b
+mul=a*b
+di=a/b
+print("Addition=",ad)
+print("Subtraction=",su)
+print("Multiplication =",mul)
+print("Division=",di)
